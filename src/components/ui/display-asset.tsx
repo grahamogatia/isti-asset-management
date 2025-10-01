57a2074f0f8d7b8aee86d890fee8cd040698b2ca
grahamogatia <graham_joshua_ogatia@dlsu.edu.ph>
2025-10-01 11:06:42 +0800

    feat(FormFieldAssetCombobox): implement asset selection combobox with search functionality

--- src/components/ui/display-asset.tsx
+++ src/components/ui/display-asset.tsx
@@ -1,14 +1,14 @@
-function DisplayType({category, sub_category, type}: {category: string, sub_category: string, type: string}) {
+function DisplayAsset({asset_name, category, sub_category, type}: {asset_name: string, category: string, sub_category: string, type: string}) {
     return (
         <div className="flex flex-col justify-start">
             <span className="font-medium text-gray-900 text-sm leading-tight">
-                {type}
+                {asset_name}
             </span>
             <span className="text-xs text-gray-500 leading-tight">
-                {category} › {sub_category}
+                {category} › {sub_category} › {type}
             </span>
         </div>
     );
 }
- 
-export default DisplayType;
+
+export default DisplayAsset;
