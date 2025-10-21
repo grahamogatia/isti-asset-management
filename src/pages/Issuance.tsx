4db0866165397f5f967dfdc466945e6e73096ff7
grahamogatia <graham_joshua_ogatia@dlsu.edu.ph>
2025-10-21 11:24:50 +0800

    refactor: implement tabbed navigation in Issuance component for better data display

--- src/pages/Issuance.tsx
+++ src/pages/Issuance.tsx
@@ -1,3 +1,4 @@
+import DisplayTabsByStatus from "@/components/layout/DisplayTabsByStatus";
 import IssuanceForm from "@/components/pages/forms/create/IssuanceForm";
 import { DataTable } from "@/components/ui/data-table";
 import {
@@ -5,23 +6,48 @@ import {
   issuance_filters,
   useIssuanceColumns,
 } from "@/data/issuance_columns";
+import type { Tab } from "@/data/types";
 import { useIssuances } from "@/hooks/useIssuance";
+import { useLookupFunctions } from "@/hooks/useLookupFunctions";
+import { useMemo, useState } from "react";
+
+const ISSUANCE_TABS: Tab[] = [
+  { label: "All", value: "All" },
+  { label: "Pulled Out", value: "Pulled Out" },
+];
 
 function Issuance() {
+  const [selectedStatus, setSelectedStatus] = useState<string>("All");
   const { issuance_columns } = useIssuanceColumns();
   const { data: issuances } = useIssuances();
+  const { getStatusIdGivenStatusName } = useLookupFunctions();
+  const pulledOutId = getStatusIdGivenStatusName("Issuance", "Pulled Out");
+
+  const displayedIssuances = useMemo(() => {
+    if (selectedStatus === "Pulled Out") {
+      return issuances?.filter(
+        (issuance) => issuance.status_id === pulledOutId
+      );
+    } else {
+      return issuances;
+    }
+  }, [issuances, selectedStatus, pulledOutId]);
 
   return (
-    <div className="mx-auto w-full border rounded-xl py-3.5 p-5">
+    <DisplayTabsByStatus
+      selectedStatus={selectedStatus}
+      setSelectedStatus={setSelectedStatus}
+      tabs={ISSUANCE_TABS}
+    >
       <DataTable
         columns={issuance_columns}
-        data={issuances ?? []}
+        data={displayedIssuances ?? []}
         defaultVisibleColumns={def_issuance_columns}
         filterableColumns={issuance_filters}
         type="Issuance"
         form={<IssuanceForm />}
       />
-    </div>
+    </DisplayTabsByStatus>
   );
 }
 
