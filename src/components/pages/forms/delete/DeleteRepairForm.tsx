d1922d4a7f2bf38bd87b2b11ecdc379957144670
grahamogatia <graham_joshua_ogatia@dlsu.edu.ph>
2025-10-21 14:09:37 +0800

    feat: enhance DeleteRepairForm to include success and error notifications on deletion

--- src/components/pages/forms/delete/DeleteRepairForm.tsx
+++ src/components/pages/forms/delete/DeleteRepairForm.tsx
@@ -1,22 +1,27 @@
 import DeleteDialog from "@/components/layout/DeleteDialog";
 import type { Repair } from "@/data/types";
 import { useDeleteRepair } from "@/hooks/useRepair";
+import { toast } from "sonner";
 
 interface DeleteRepairFormProps {
   repair: Repair;
 }
 
 function DeleteRepairForm({ repair }: DeleteRepairFormProps) {
-  
   const { mutate } = useDeleteRepair();
 
   const handleDeleteRepair = () => {
-    mutate(repair.repair_request_id as number);
-  }
+    mutate(repair.repair_request_id as number, {
+      onSuccess: () => {
+        toast.info("Repair request deleted successfully");
+      },
+      onError: () => {
+        toast.error("Failed to delete repair request");
+      },
+    });
+  };
 
-  return (
-    <DeleteDialog handleConfirm={handleDeleteRepair}/>
-  );
+  return <DeleteDialog handleConfirm={handleDeleteRepair} />;
 }
 
 export default DeleteRepairForm;
