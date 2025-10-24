ac4697df0ee381d2e9ef1dc0cad448f7d563e2f6
grahamogatia <graham_joshua_ogatia@dlsu.edu.ph>
2025-10-24 10:41:16 +0800

    feat: add rollup-plugin-visualizer for bundle analysis and improve config structure

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,25 @@
 import { defineConfig } from "vite";
+import { visualizer } from "rollup-plugin-visualizer";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    visualizer({
+      open: false, // Automatically opens the report
+      gzipSize: true, // Show gzip sizes
+      brotliSize: true, // Show brotli sizes
+      filename: "stats.html",
+    }),
+  ],
   resolve: {
-		alias: {
-			"@": path.resolve(__dirname, "./src"),
-		},
-	},
-  server: {port: 9917}
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  server: { port: 9917 },
 });
