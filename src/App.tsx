fef2d38642416ef87e6f961585be24b318dc2271
grahamogatia <graham_joshua_ogatia@dlsu.edu.ph>
2025-11-19 10:35:27 +0800

    refactor: implement lazy loading for routes to improve performance

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,21 @@
-import { Route, Routes } from "react-router-dom";
 import Header from "./components/layout/Header";
 import Sidebar from "./components/layout/Sidebar";
-import Dashboard from "./pages/Dashboard";
-import Assets from "./pages/Assets";
-import Borrow from "./pages/Borrow";
-import Issuance from "./pages/Issuance";
-import Repair from "./pages/Repair";
+import { Route, Routes } from "react-router-dom";
 import { useSidebar } from "./components/ui/sidebar";
 import { cn } from "./lib/utils";
-import Settings from "./pages/Settings";
-import CategoriesPage from "./components/pages/settings/categories/CategoriesPage";
-import AssetConfigPage from "./components/pages/settings/AssetConfigPage";
-import RecycleBinPage from "./components/pages/settings/RecycleBinPage";
-import InsurancePage from "./components/pages/settings/InsurancePage";
-import AssetBatchUploadPage from "./components/pages/assets/AssetBatchUploadPage";
+import { lazy, Suspense } from "react";
+
+const Dashboard = lazy(() => import("@/pages/Dashboard"));
+const Assets = lazy(() => import("@/pages/Assets"));
+const Borrow = lazy(() => import("@/pages/Borrow"));
+const Issuance = lazy(() => import("@/pages/Issuance"));
+const Repair = lazy(() => import("@/pages/Repair"));
+const Settings = lazy(() => import("@/pages/Settings"));
+const CategoriesPage = lazy(() => import("@/components/pages/settings/categories/CategoriesPage"));
+const AssetConfigPage = lazy(() => import("@/components/pages/settings/AssetConfigPage"));
+const RecycleBinPage = lazy(() => import("@/components/pages/settings/RecycleBinPage"));
+const InsurancePage = lazy(() => import("@/components/pages/settings/InsurancePage"));
+const AssetBatchUploadPage = lazy(() => import("@/components/pages/assets/AssetBatchUploadPage"));
 
 function App() {
   return (
@@ -43,9 +45,16 @@ function AppRoutes() {
           )}
         >
           <Routes>
-            <Route path="/" element={<Dashboard />} />
-            <Route path="assets" element={<Assets />}/>
-            <Route path="assets/batch" element={<AssetBatchUploadPage />}/>
+            <Route
+              path="/"
+              element={
+                <Suspense fallback={<>Loading...</>}>
+                  <Dashboard />
+                </Suspense>
+              }
+            />
+            <Route path="assets" element={<Assets />} />
+            <Route path="assets/batch" element={<AssetBatchUploadPage />} />
             <Route path="borrow" element={<Borrow />} />
             <Route path="issuance" element={<Issuance />} />
             <Route path="repair" element={<Repair />} />
